fdd18fbaea3fa32a2f8b9858d4c6b1ee6627a3d5
vscide <resatgergin26@gmail.com>
2025-04-11 23:17:40 +0400

    Update nethunter-fix.sh

--- nethunter-fix.sh
+++ nethunter-fix.sh
@@ -1,20 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# NetHunter fix script
-clear
-echo -e "\033[1;36m[+] NetHunter fix başlatılıyor...\033[0m"
+# NetHunter Fix
+echo -e "${cyan}[+] NetHunter GUI (KeX) fix uygulanıyor...${reset}"
 
-# Gerekli paketleri yükleyin
-pkg update -y
-pkg install -y nethunter
+apt update -y
+apt install -y tigervnc-standalone-server
 
-# NetHunter kurulumu yapın
-echo -e "\033[1;32m[+] NetHunter başarıyla kuruldu.\033[0m"
+# VNC sunucusunu sıfırla
+vncserver -kill :1 >/dev/null 2>&1
+rm -rf ~/.vnc
+vncserver
 
-# NetHunter GUI'yi başlatmaya çalışın
-echo -e "\033[1;36m[+] NetHunter GUI başlatılıyor...\033[0m"
-nethunter kex &
+echo -e "${green}[✓] VNC sunucusu yeniden başlatıldı.${reset}"
 
-# NetHunter CLI başlatma
-echo -e "\033[1;32m[+] NetHunter CLI'yi başlatın:\033[0m"
-echo -e "\033[1;33m$ nethunter\033[0m"
+# chmod +x
+chmod +x kali.sh
+chmod +x nethunter-fix.sh
+chmod +x ~/.proot-distro/distro/zexkali.sh
